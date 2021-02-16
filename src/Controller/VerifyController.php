4f3b1dd1a5655cb5f6db5872e2a8c95c427967b4
chrisvenator <christopher.scherling@gmail.com>
2021-02-16 12:33:19 +0100

    Verify User fix

--- src/Controller/VerifyController.php
+++ src/Controller/VerifyController.php
@@ -21,31 +21,31 @@ class VerifyController extends AbstractController {
      * @return Response
      */
     public function Verify_User(string $id, Request $request, SerializerInterface $serializer): Response {
-        if ($request->getMethod() == 'POST') {
-            $data = json_decode($request->getContent(), true);
-            $entityManager = $this->getDoctrine()->getManager();
-            $code = $data['code'] ?? $id ?? null;
-            if (!isset($code)) return new Response("please provide a code", 400);
+//        if ($request->getMethod() == 'POST') {
+        $data = json_decode($request->getContent(), true);
+        $entityManager = $this->getDoctrine()->getManager();
+        $code = $data['code'] ?? $id ?? null;
+        if (!isset($code)) return new Response("please provide a code", 400);
 
-            $verify = $this->getDoctrine()->getRepository(Verify::class)->findBy(['code' => $code]);
-            if (count($verify) != 1) return new Response("CODE NOT FOUND", 404);
+        $verify = $this->getDoctrine()->getRepository(Verify::class)->findBy(['code' => $code]);
+        if (count($verify) != 1) return new Response("CODE NOT FOUND", 404);
 
-            $user = $this->getDoctrine()->getRepository(User::class)->findBy(['id' => $verify[0]->getFKUserID()]);
-            if (count($user) != 1) return new Response("USER NOT FOUND", 404);
-            $user = $user[0];
-            if ($user->getVerified()) return new Response("User is already verified", 400);
+        $user = $this->getDoctrine()->getRepository(User::class)->findBy(['id' => $verify[0]->getFKUserID()]);
+        if (count($user) != 1) return new Response("USER NOT FOUND", 404);
+        $user = $user[0];
+        if ($user->getVerified()) return new Response("User is already verified", 400);
 
-            $verify[0]->setVerfiyDate(new \DateTime());
-            $entityManager->persist($verify[0]);
-            $entityManager->flush();
+        $verify[0]->setVerfiyDate(new \DateTime());
+        $entityManager->persist($verify[0]);
+        $entityManager->flush();
 
 
-            $user->setVerified(true);
-            $entityManager->persist($user);
-            $entityManager->flush();
+        $user->setVerified(true);
+        $entityManager->persist($user);
+        $entityManager->flush();
 
-            return new Response($serializer->serialize($user, 'json'), 200);
-        }
-        return new Response("Successfuly verified!");
+        return new Response($serializer->serialize($user, 'json'), 200);
+//        }
+//        return new Response("Successfuly verified!: " . $serializer->serialize($user, 'json'));
     }
 }
