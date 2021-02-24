bc8cbc1832d8f26ec6d8f80e122c73390ce05fb2
Chrisvenator <5238@htl.rennweg.at>
2021-02-24 13:17:23 +0100

    Neue Frontendversion

--- public/js/chunk-2d0ba1e4.91a8862b.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ba1e4"],{"369d":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},c=[],r={name:"CashierQrCode"},s=r,u=t("2877"),i=Object(u["a"])(s,a,c,!1,null,"5913c924",null);n["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d0ba1e4.91a8862b.js.map
