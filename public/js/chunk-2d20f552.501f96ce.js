252583418c1e1a7e48deab63b29c94cdd5715c37
chrisvenator <christopher.scherling@gmail.com>
2021-02-26 14:31:36 +0100

    Neues Frontend Teil 1

--- public/js/chunk-2d20f552.501f96ce.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20f552"],{b2d0:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},r=[],a={name:"TermsAndServiceCompany"},s=a,u=t("2877"),i=Object(u["a"])(s,c,r,!1,null,"29928b48",null);n["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d20f552.501f96ce.js.map
