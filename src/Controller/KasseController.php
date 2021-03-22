84b4b1e325315d9ca89e81881d24e78a2aabc757
chrisvenator <christopher.scherling@gmail.com>
2021-03-22 18:26:26 +0100

    Kassen hinzufügen + Abfragen

--- src/Controller/KasseController.php
+++ src/Controller/KasseController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Firma;
+use App\Entity\Kasse;
 use App\Entity\User;
 use App\Service\DSGVO;
 use Doctrine\ORM\EntityManager;
@@ -39,5 +41,65 @@ class KasseController extends AbstractController {
 //        $erg = $dsgvo->deleteEverything(null, '32541432');
 
         return new Response($serializer->serialize($erg, 'json'), 404);
+    }
+
+    /**
+     * @Route("/api/addRegisters")
+     * @param Request $request
+     * @return Response
+     */
+    public function ADD_Kasse_API(Request $request, SerializerInterface $serializer, Hash $jsonAuth): Response {
+        if ($request->getMethod() == 'POST') {
+            $entityManager = $this->getDoctrine()->getManager();
+            $data = json_decode($request->getContent(), true);
+            $hash = $data['hash'];
+            if (!$jsonAuth->checkJsonCode($data['hash'])) return new Response('Hash Invalid', 403);
+
+            $name = $data["qrcode"];
+            $firmenname = $data['companyName'];
+
+            $FIRMA = $this->getDoctrine()->getRepository(Firma::class)->findBy(['Firmanname' => $firmenname]);
+            if (count($FIRMA) == 0) return new Response("This company does not exist!", 400);
+            $FIRMA = $FIRMA[0];
+
+            $rechte = $jsonAuth->returnRechteFromHash($hash, $firmenname);
+
+            if ($rechte >= 2) {
+                $KASSE = new Kasse();
+                $KASSE->setBezeichnung($name);
+                $KASSE->setFkFirmaId($FIRMA->getId());
+
+                $entityManager->persist($KASSE);
+                $entityManager->flush();
+            } else {
+                return new Response("You do not have enough rights!", 400);
+            }
+
+            $Kassen = $this->getDoctrine()->getRepository(Kasse::class)->findBy(['fk_firma_id' => $FIRMA->getID()]);
+
+            return new Response($Kassen, 200);
+        }
+    }
+
+    /**
+     * @Route("/api/getRegisters")
+     * @param Request $request
+     * @return Response
+     */
+    public function GET_Kasse_API(Request $request, SerializerInterface $serializer, Hash $jsonAuth): Response {
+        if ($request->getMethod() == 'POST') {
+            $data = json_decode($request->getContent(), true);
+            if (!$jsonAuth->checkJsonCode($data['hash'])) return new Response('Hash Invalid', 403);
+
+            $firmenname = $data['companyName'];
+
+            $FIRMA = $this->getDoctrine()->getRepository(Firma::class)->findBy(['Firmanname' => $firmenname]);
+            if (count($FIRMA) == 0) return new Response("Company not found", 400);
+            $FIRMA = $FIRMA[0];
+
+            $KASSE = $this->getDoctrine()->getRepository(Kasse::class)->findBy(['fk_firma_id' => $FIRMA->getID()]);
+
+            return new Response($serializer->serialize($KASSE, 'json'), 200);
+        }
     }
 }
