2f8faf9b64cd1c921c35bebb5e3336408e11b10f
chrisvenator <christopher.scherling@gmail.com>
2021-03-22 18:59:56 +0100

    kasse fix 2

--- src/Controller/KasseController.php
+++ src/Controller/KasseController.php
@@ -15,33 +15,6 @@ use App\Service\Hash;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class KasseController extends AbstractController {
-    /**
-     * @Route("/kasse", name="kasse")
-     */
-//    public function index()
-//    {
-//        return $this->render('kasse/index.php.twig', [
-//            'controller_name' => 'KasseController',
-//        ]);
-//    }
-    /**
-     * @Route("/api/VerifyCode", format="json", requirements={ "_format": "html|json" })
-     * @param Request $request
-     * @return Response
-     */
-    public function TestingGrounds(Request $request, SerializerInterface $serializer, Hash $jsonAuthentification, DSGVO $dsgvo): Response {
-        $entityManager = $this->getDoctrine()->getManager();
-//        $jsonAuthentification->saveJsonCode(2);
-        $erg = "";
-        $hash = "ttgIeo6YNCoDg3YoJNUlMy868vhWBblZv6z4Ki61pEV3ATcWiqr4aZPPLiu19MfNHl5wa48tnJM6l2N7iAJumg7mnw6z0kGIhGtS";
-//        $erg = $jsonAuthentification->checkJsonCode($hash);
-//        $erg = $Users = $this->getDoctrine()->getRepository(User::class)->findBy(['email' => 'test'])[0];
-//        $erg = $jsonAuthentification->returnRechteFromHash($hash, "Schnitzelbude1337");
-//        $erg = $dsgvo->unlockUser(null, '5124@htl.rennweg.at');
-//        $erg = $dsgvo->deleteEverything(null, '32541432');
-
-        return new Response($serializer->serialize($erg, 'json'), 404);
-    }
 
     /**
      * @Route("/api/addRegister")
