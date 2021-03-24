c70f54e58293893732ec9c561087c0198d82d2b3
chrisvenator <christopher.scherling@gmail.com>
2021-03-24 19:14:59 +0100

    Design fix 9

--- src/Controller/FirmaController.php
+++ src/Controller/FirmaController.php
@@ -186,7 +186,7 @@ class FirmaController extends AbstractController {
                     'files' => $designs,
                     'design' => [
                         'logo' => $firma->getDatei(),
-                        'design' => $farbe[0]
+                        'colorPallet' => $farbe[0]
                     ]
 
                 ];
