1b6eb61eaa7b09c48f99bab6246f335bc515bd63
chrisvenator <christopher.scherling@gmail.com>
2021-03-25 23:58:45 +0100

    neue frontendversion 2

---
+++ public/js/chunk-2d0deb55.5043d6f6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0deb55"],{8799:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},u=[],a={name:"Company"},c=a,o=t("2877"),s=Object(o["a"])(c,r,u,!1,null,"394bb2fe",null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0deb55.5043d6f6.js.map
