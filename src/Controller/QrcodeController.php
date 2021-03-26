6d5c89e35311990a356ac97ae0288de517960953
chrisvenator <christopher.scherling@gmail.com>
2021-03-26 12:34:28 +0100

    qr-code fix: no duplicated codes: debug 3

--- src/Controller/QrcodeController.php
+++ src/Controller/QrcodeController.php
@@ -140,7 +140,7 @@ class QrcodeController extends AbstractController {
             } else {
                 $savedPoints = $this->saveCode($OGCode, $UserID);
                 if ($savedPoints == "-1 Kassa") return new Response("Kassa not found", 400);
-                return new Response($serializer->serialize([$OGCode, $savedPoints, $this->getDoctrine()->getRepository(Qrcode::class)->findAll()], 'json'), 200);
+                return new Response($serializer->serialize([$OGCode, $savedPoints, $this->getDoctrine()->getRepository(Qrcode::class)->findAll(), count($this->getDoctrine()->getRepository(Qrcode::class)->findBy(['Klartext' => $OGCode]))], 'json'), 200);
             }
         } else {
             return new Response("", 404);
